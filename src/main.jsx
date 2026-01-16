8030149ad1deb25331f37a1a2a321680a202e2f2
Julia Alkhimova <devalchem@gmail.com>
2026-01-16 19:32:04 +0100

    feat: connect all created styles to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/reset.css'
 import './styles/tokens.css'
-import './index.css'
+import './styles/theme.css'
+import './styles/theme.css'
+import './styles/global.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
