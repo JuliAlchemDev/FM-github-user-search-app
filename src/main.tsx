bb710a5d4167fd890bbf72ccfe5eadf392d035ef
Julia Alkhimova <devalchem@gmail.com>
2026-02-27 09:47:01 +0100

    refactor(root): change App and main to .tsx to complete migration

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './styles/reset.css'
 import './styles/tokens.css'
 import './styles/theme.css'
 import './styles/theme.css'
 import './styles/global.css'
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import App from './App';
+const rootElement = document.getElementById('root')!;
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
