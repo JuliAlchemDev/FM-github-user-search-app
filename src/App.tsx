51e0e5f3ed048b71f1b843c57cd4529ee3953b67
Julia Alkhimova <devalchem@gmail.com>
2026-03-01 11:40:24 +0100

    feat(app): add ProfileCard to app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <SearchBar />
+      <ProfileCard />
     </>
   );
 };
