395f9192be01a7a505eb8706ab4f273de21d7a74
Julia Alkhimova <devalchem@gmail.com>
2026-03-06 15:22:57 +0100

    feat(app): connect gitHub Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ProfileCard from "./components/ProfileCard/ProfileCard";
+import { GitHubUser } from "./types/github";
+import { mockUser } from "./mocks/github.mock";
 
-const App: React.FC = () => {
+
+
+const App: React.FC = () => { 
+  const [user, setUser] = useState<GitHubUser | null>(mockUser);
+  const [error, setError] = useState<string>("");
+
+  const handleSearch = async (username: string) => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        setUser(null);
+        setError("No results");
+      } else {
+        const data = await response.json();
+        setUser(data);
+        setError("");
+      }
+    } catch {
+      setUser(null);
+      setError("No results");
+    }
+  };
   return (
     <>
       <Header />
-      <SearchBar />
-      <ProfileCard />
+      <SearchBar onSearch={handleSearch} error={error}/>
+      <ProfileCard user={user} />
     </>
   );
 };
