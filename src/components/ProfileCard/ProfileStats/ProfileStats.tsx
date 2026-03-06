01c991358c4a3c6d13836bb1ff8dbea57706d97c
Julia Alkhimova <devalchem@gmail.com>
2026-03-06 08:31:34 +0100

    feat(profileCard): create and define structure and styles for ProfileStats section

--- src/components/ProfileCard/ProfileStats/ProfileStats.tsx
+++ src/components/ProfileCard/ProfileStats/ProfileStats.tsx
@@ -1,4 +1,4 @@
-import styles from "./ProfileCard.module.css";
+import styles from "./ProfileStats.module.css";
 
 type ProfileStatsProps = {
     public_repos: number | null,
@@ -6,23 +6,22 @@ type ProfileStatsProps = {
     following: number | null,
 }
 
-const profileStats = ({public_repos,followers, following}: ProfileStatsProps) => {
-  [];
+const ProfileStats = ({public_repos,followers, following}: ProfileStatsProps) => {
   return (
     <dl className={styles.profileStats}>
       <div className={styles.statItem}>
         <dt className={styles.statLabel}>Repos</dt>
-        <dd className={styles.statValue}>{public_repos}</dd>
+        <dd className={styles.statValue}>{public_repos ?? 0}</dd>
       </div>
       <div className={styles.statItem}>
         <dt className={styles.statLabel}>Followers</dt>
-        <dd className={styles.statValue}>{followers}</dd>
+        <dd className={styles.statValue}>{followers ?? 0}</dd>
       </div>
       <div className={styles.statItem}>
         <dt className={styles.statLabel}>Following</dt>
-        <dd className={styles.statValue}>{following}</dd>
+        <dd className={styles.statValue}>{following ?? 0}</dd>
       </div>
     </dl>
   );
 };
-export default profileStats;
+export default ProfileStats;
