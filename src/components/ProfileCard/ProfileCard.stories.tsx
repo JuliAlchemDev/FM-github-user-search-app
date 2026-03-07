ced4781c8d27e5ce4d691da9517099fa4aa26ea1
Julia Alkhimova <devalchem@gmail.com>
2026-03-07 09:07:29 +0100

    feat(profileCard): add ProfileCard stories for light theme as a default, dark theme and no results state

---
+++ src/components/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import ProfileCard from "./ProfileCard";
+import { mockUser } from "../../mocks/github.mock";
+
+const meta = {
+  title: "Components/ProfileCard",
+  component: ProfileCard,
+  tags: ["autodocs"],
+  parameters: { layout: "padded" },
+} satisfies Meta<typeof ProfileCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    user: mockUser,
+  },
+};
+export const DarkTheme: Story = {
+  parameters: { theme: "dark" },
+  args: {
+    user: mockUser,
+  },
+};
+export const NoResults: Story = {
+    args: {
+    user: null,
+  },
+};
