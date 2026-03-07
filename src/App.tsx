c0f2cfae777cbf6585eda68a3a9b66010dd14cb6
Julia Alkhimova <devalchem@gmail.com>
2026-03-07 11:26:17 +0100

    feat(app): add loading state to improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import ProfileCard from "./components/ProfileCard/ProfileCard";
 import { GitHubUser } from "./types/github";
 import { mockUser } from "./mocks/github.mock";
+import Loader from "./components/UI/Loader/Loader";
 
-
-
-const App: React.FC = () => { 
+const App: React.FC = () => {
   const [user, setUser] = useState<GitHubUser | null>(mockUser);
   const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (username: string) => {
+    setIsLoading(true);
+    const minDelay = new Promise((resolve) => setTimeout(resolve, 2000));
+
     try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
+      const [response] = await Promise.all([
+        fetch(`https://api.github.com/users/${username}`),
+        minDelay,
+      ]);
+
       if (!response.ok) {
         setUser(null);
         setError("No results");
@@ -25,13 +32,15 @@ const App: React.FC = () => {
     } catch {
       setUser(null);
       setError("No results");
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
     <>
       <Header />
-      <SearchBar onSearch={handleSearch} error={error}/>
-      <ProfileCard user={user} />
+      <SearchBar onSearch={handleSearch} error={error} />
+      {isLoading ? <Loader /> : <ProfileCard user={user} />}
     </>
   );
 };
