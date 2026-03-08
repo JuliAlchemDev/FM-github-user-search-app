0ea0b2deb8fd1726a9bcc1732d60f781149875b0
Julia Alkhimova <devalchem@gmail.com>
2026-03-08 10:58:19 +0100

    feat(themeToggle): add ClickToggle story to test correct behavior

--- src/components/Header/ThemeToggle/ThemeToggle.stories.tsx
+++ src/components/Header/ThemeToggle/ThemeToggle.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { within, userEvent, expect } from 'storybook/test';
 import ThemeToggle from "./ThemeToggle";
 
 const meta = {
@@ -18,4 +19,21 @@ export const LightTheme: Story = {
 export const DarkTheme: Story = {
   parameters: { theme: 'dark' },
   args: { defaultDark: true },
+};
+
+export const ClickToggle: Story = {
+  args: { defaultDark: false },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button');
+
+    await expect(canvas.getByLabelText('moon icon')).toBeInTheDocument()
+    await expect(button).toHaveTextContent('Dark');
+    
+    await userEvent.click(button);
+    
+    await expect(button).toHaveTextContent('Light');
+    await expect(canvas.getByLabelText('sun icon')).toBeInTheDocument()
+    await expect(canvas.queryByLabelText('moon icon')).not.toBeInTheDocument()
+  }
 };
