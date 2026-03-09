3aed36823d7942679cc4b47a0c9c6c7ae54ce34f
Julia Alkhimova <devalchem@gmail.com>
2026-03-09 08:25:20 +0100

    test(searchBar): add withSearch and noResults test to check behavior

--- src/components/SearchBar/SearchBar.stories.tsx
+++ src/components/SearchBar/SearchBar.stories.tsx
@@ -1,24 +1,45 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { within, userEvent, waitFor, expect } from "storybook/test";
 import SearchBar from "./SearchBar";
 
 const meta = {
-    title: "Components/SearchBar",
-    component: SearchBar,
-    tags: ["autodocs"],
-    parameters: {layout: "padded"},
+  title: "Components/SearchBar",
+  component: SearchBar,
+  tags: ["autodocs"],
+  parameters: { layout: "padded" },
 } satisfies Meta<typeof SearchBar>;
 
 export default meta;
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
-export const LightTheme: Story = { };
+export const LightTheme: Story = {};
 export const LightThemeWithError: Story = {
-    args: {error: "No results"}
- };
+  args: { error: "No results" },
+   play: async ({canvasElement}) => {
+        const canvas = within(canvasElement);
+        
+        await expect(canvas.getByText('No results')).toBeInTheDocument();
+    }
+};
 export const DarkTheme: Story = {
-  parameters: { theme: 'dark' },
+  parameters: { theme: "dark" },
 };
 export const DarkThemeWithError: Story = {
-    parameters: { theme: 'dark' },
-    args: {error: "No results"},
- };
+  parameters: { theme: "dark" },
+  args: { error: "No results" },
+   play: async ({canvasElement}) => {
+        const canvas = within(canvasElement);
+        
+        await expect(canvas.getByText('No results')).toBeInTheDocument();
+    }
+};
+
+export const WithSearch: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.type(canvas.getByRole("textbox"), "octocat");
+
+    await userEvent.click(canvas.getByRole("button", { name: /search/i }));
+  },
+};
